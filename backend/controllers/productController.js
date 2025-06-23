ef21444058226fb1a79174bd00e3deae924d8011
NemindaBandara <nemindabandara15@gmail.com>
2025-06-23 23:17:00 +0530

    product done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -67,33 +67,43 @@ const addProduct = async (req, res) => {
 
     res.json({ success: true, message: "Product Added" });
   } catch (error) {
+    console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
 // function for list products
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for remove products
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Product Deleted" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { listProducts, addProduct, removeProduct, singleProduct };
-
-//fggh
-//new
-//lines
-//added
-//delete
-//later
-//just
-//adding
-//new
-//ff
-//gg
-//kkkk
-//llll
-//hhhh
-//lkj
