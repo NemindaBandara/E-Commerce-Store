2759e96cd3bc084397b78a152019a49f432353fd
NemindaBandara <nemindabandara15@gmail.com>
2025-07-13 01:52:51 +0530

    add completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders'
 import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
 
@@ -31,9 +31,9 @@ const App = () => {
         <Sidebar />
         <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
         <Routes>
-          <Route path='/add' setToken={setToken} element={<Add />} />
-          <Route path='/list' setToken={setToken} element={<List />} />
-          <Route path='/orders' setToken={setToken} element={<Orders />} />
+          <Route path='/add' element={<Add token={token} />} />
+          <Route path='/list' element={<List token={token} />} />
+          <Route path='/orders' element={<Orders token={token} />} />
         </Routes>
         </div>
       </div>
